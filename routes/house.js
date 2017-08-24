9ace6b4025e0a0242d975b4746a1a23ca9facbe0
Zachary Morel <morelz133@gmail.com>
2017-08-24 19:22:33 -0400

    Updating Home CSS

--- routes/house.js
+++ routes/house.js
@@ -24,6 +24,7 @@ module.exports = app => {
     })
   })
 
+  // DISPLAYS SORTED HOUSE CHARACTERS
   app.get('/GRHS/Hufflepuff', (request, response) => {
     Character.find({ house: 'Hufflepuff' })
       .then(characters => {
